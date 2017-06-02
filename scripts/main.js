872d6fc42e72e68f8688cc6b34d5af471c0eeb77
Pritvora <ghostshooter83@yandex.ru>
2017-06-02 21:59:01 +0300

    x7tp0,.[.

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+function getCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+$(document).ready(function () {
+    var books = [{
+        name: "",
+        pik: "",
+        grade: 2
+    }];
+    var filter = "";
+    var gradeFilter = getCookie("grade") || 2;
+    $(".sclass").click(function () {
+        gradeFilter = $(this).data("grade");
+        $(".selected.sclass").removeClass("selected");
+        $(this).addClass("selected");
+        var date = new Date(new Date().getTime() + 60 * 1000);
+        document.cookie = "grade=" + gradeFilter + "; path=/; expires=" + date.toUTCString();
+    });
+});
